a1cd6842135eb03bacefc46409b5e59d51014db8
Sahim <loris.cangini@hotmail.it>
2016-09-16 20:52:56 +0200

    Fixed "Ancient Gear Reborn"

--- script/c7570.lua
+++ script/c7570.lua
@@ -7,10 +7,7 @@ function c7570.initial_effect(c)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetHintTiming(0,TIMING_END_PHASE)
-	e1:SetCountLimit(1)
-	e1:SetCondition(c7570.condition)
-	e1:SetTarget(c7570.target)
-	e1:SetOperation(c7570.operation)
+	e1:SetTarget(c7570.acttarget)
 	c:RegisterEffect(e1)
 	--summon
 	local e2=Effect.CreateEffect(c)
@@ -18,7 +15,7 @@ function c7570.initial_effect(c)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
-	e2:SetCountLimit(1)
+	e2:SetCountLimit(1,7570)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetHintTiming(0,TIMING_END_PHASE)
@@ -27,14 +24,32 @@ function c7570.initial_effect(c)
 	e2:SetOperation(c7570.operation)
 	c:RegisterEffect(e2)
 end
+function c7570.acttarget(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return c7570.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc) end
+	if chk==0 then return true end
+	if c7570.condition(e,tp,eg,ep,ev,re,r,rp) and c7570.target(e,tp,eg,ep,ev,re,r,rp,0) and Duel.SelectYesNo(tp,aux.Stringid(39387565,0)) then
+		e:SetCategory(CATEGORY_SPECIAL_SUMMON)
+		e:SetProperty(EFFECT_FLAG_CARD_TARGET)
+		e:SetOperation(c7570.operation)
+		c7570.target(e,tp,eg,ep,ev,re,r,rp,1)
+	else
+		e:SetCategory(0)
+		e:SetProperty(0)
+		e:SetOperation(nil)
+	end
+end
+
 function c7570.condition(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0
 end
 function c7570.filter(c,e,tp)
 	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x7) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c7570.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c7570.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+function c7570.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c7570.filter(chkc,e,tp) end
+	if chk==0 then return Duel.IsExistingMatchingCard(c7570.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
+		and Duel.GetFlagEffect(tp,7570)==0 end
+	Duel.RegisterFlagEffect(tp,7570,RESET_PHASE+PHASE_END,0,1)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectTarget(tp,c7570.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
