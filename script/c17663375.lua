44b82b03d8554b38dfb4c197fa15ec98c51a26e1
EerieCode <eeriecode@gmail.com>
2016-09-23 22:20:33 +0200

    Official numbers for SR03

--- script/c17663375.lua
+++ script/c17663375.lua
@@ -1,17 +1,17 @@
 --古代の機械飛竜
 --Ancient Gear Wyvern
 --Scripted by Eerie Code
-function c7453.initial_effect(c)
+function c17663375.initial_effect(c)
 	--search
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(7453,0))
+	e1:SetDescription(aux.Stringid(17663375,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
-	e1:SetCountLimit(1,7453)
-	e1:SetTarget(c7453.thtg)
-	e1:SetOperation(c7453.thop)
+	e1:SetCountLimit(1,17663375)
+	e1:SetTarget(c17663375.thtg)
+	e1:SetOperation(c17663375.thop)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
@@ -23,19 +23,19 @@ function c7453.initial_effect(c)
 	e3:SetCode(EFFECT_CANNOT_ACTIVATE)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetTargetRange(0,1)
-	e3:SetValue(c7453.aclimit)
-	e3:SetCondition(c7453.actcon)
+	e3:SetValue(c17663375.aclimit)
+	e3:SetCondition(c17663375.actcon)
 	c:RegisterEffect(e3)
 end
 
-function c7453.filter(c)
-	return c:IsSetCard(0x7) and not c:IsCode(7453) and c:IsAbleToHand()
+function c17663375.filter(c)
+	return c:IsSetCard(0x7) and not c:IsCode(17663375) and c:IsAbleToHand()
 end
-function c7453.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c7453.filter,tp,LOCATION_DECK,0,1,nil) end
+function c17663375.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c17663375.filter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function c7453.thop(e,tp,eg,ep,ev,re,r,rp)
+function c17663375.thop(e,tp,eg,ep,ev,re,r,rp)
 	--cannot set
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
@@ -53,22 +53,22 @@ function c7453.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.RegisterEffect(e3,tp)
 	local e4=e1:Clone()
 	e4:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	e4:SetTarget(c7453.sumlimit)
+	e4:SetTarget(c17663375.sumlimit)
 	Duel.RegisterEffect(e4,tp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,c7453.filter,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,c17663375.filter,tp,LOCATION_DECK,0,1,1,nil)
 	if g:GetCount()>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
 	end
 end
-function c7453.sumlimit(e,c,sump,sumtype,sumpos,targetp)
+function c17663375.sumlimit(e,c,sump,sumtype,sumpos,targetp)
 	return bit.band(sumpos,POS_FACEDOWN)~=0
 end
 
-function c7453.aclimit(e,re,tp)
+function c17663375.aclimit(e,re,tp)
 	return (re:IsActiveType(TYPE_MONSTER) and not re:GetHandler():IsImmuneToEffect(e))
 end
-function c7453.actcon(e)
+function c17663375.actcon(e)
 	return Duel.GetAttacker()==e:GetHandler()
 end
--- script/c44874522.lua
+++ script/c44874522.lua
@@ -1,19 +1,19 @@
 --古代の機械熱核竜
 --Ancient Gear Reactor Dragon
 --Scripted by Eerie Code
-function c7451.initial_effect(c)
+function c44874522.initial_effect(c)
 	--mat check
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_MATERIAL_CHECK)
-	e1:SetValue(c7451.valcheck)
+	e1:SetValue(c44874522.valcheck)
 	c:RegisterEffect(e1)
 	--summon success
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EVENT_SUMMON_SUCCESS)
-	e2:SetCondition(c7451.regcon)
-	e2:SetOperation(c7451.regop)
+	e2:SetCondition(c44874522.regcon)
+	e2:SetOperation(c44874522.regop)
 	c:RegisterEffect(e2)
 	e2:SetLabelObject(e1)
 	--actlimit
@@ -23,22 +23,22 @@ function c7451.initial_effect(c)
 	e3:SetCode(EFFECT_CANNOT_ACTIVATE)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetTargetRange(0,1)
-	e3:SetValue(c7451.aclimit)
-	e3:SetCondition(c7451.actcon)
+	e3:SetValue(c44874522.aclimit)
+	e3:SetCondition(c44874522.actcon)
 	c:RegisterEffect(e3)
 	--pos
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(7451,2))
+	e4:SetDescription(aux.Stringid(44874522,2))
 	e4:SetCategory(CATEGORY_DESTROY)
 	e4:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
 	e4:SetCode(EVENT_DAMAGE_STEP_END)
-	e4:SetCondition(c7451.descon)
-	e4:SetTarget(c7451.destg)
-	e4:SetOperation(c7451.desop)
+	e4:SetCondition(c44874522.descon)
+	e4:SetTarget(c44874522.destg)
+	e4:SetOperation(c44874522.desop)
 	c:RegisterEffect(e4)
 end
 
-function c7451.valcheck(e,c)
+function c44874522.valcheck(e,c)
 	local g=c:GetMaterial()
 	local flag=0
 	local tc=g:GetFirst()
@@ -54,15 +54,15 @@ function c7451.valcheck(e,c)
 	e:SetLabel(flag)
 end
 
-function c7451.regcon(e,tp,eg,ep,ev,re,r,rp)
+function c44874522.regcon(e,tp,eg,ep,ev,re,r,rp)
 	return bit.band(e:GetHandler():GetSummonType(),SUMMON_TYPE_ADVANCE)==SUMMON_TYPE_ADVANCE
 end
-function c7451.regop(e,tp,eg,ep,ev,re,r,rp)
+function c44874522.regop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local flag=e:GetLabelObject():GetLabel()
 	if bit.band(flag,0x1)~=0 then
 		local e1=Effect.CreateEffect(c)
-		e1:SetDescription(aux.Stringid(7451,0))
+		e1:SetDescription(aux.Stringid(44874522,0))
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_PIERCE)
 		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
@@ -71,7 +71,7 @@ function c7451.regop(e,tp,eg,ep,ev,re,r,rp)
 	end
 	if bit.band(flag,0x2)~=0 then
 		local e4=Effect.CreateEffect(c)
-		e4:SetDescription(aux.Stringid(7451,1))
+		e4:SetDescription(aux.Stringid(44874522,1))
 		e4:SetType(EFFECT_TYPE_SINGLE)
 		e4:SetCode(EFFECT_EXTRA_ATTACK)
 		e4:SetProperty(EFFECT_FLAG_CLIENT_HINT)
@@ -81,27 +81,27 @@ function c7451.regop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
-function c7451.aclimit(e,re,tp)
+function c44874522.aclimit(e,re,tp)
 	return re:IsHasType(EFFECT_TYPE_ACTIVATE) or (re:IsActiveType(TYPE_MONSTER) and not re:GetHandler():IsImmuneToEffect(e))
 end
-function c7451.actcon(e)
+function c44874522.actcon(e)
 	return Duel.GetAttacker()==e:GetHandler()
 end
 
-function c7451.descon(e,tp,eg,ep,ev,re,r,rp)
+function c44874522.descon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler()==Duel.GetAttacker()
 end
-function c7451.desfil(c)
+function c44874522.desfil(c)
 	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsDestructable()
 end
-function c7451.destg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c7451.desfil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
-	local g=Duel.GetMatchingGroup(c7451.desfil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+function c44874522.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c44874522.desfil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+	local g=Duel.GetMatchingGroup(c44874522.desfil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
-function c7451.desop(e,tp,eg,ep,ev,re,r,rp)
+function c44874522.desop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local tc=Duel.SelectMatchingCard(tp,c7451.desfil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	local tc=Duel.SelectMatchingCard(tp,c44874522.desfil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
 	if tc:GetCount()>0 then
 		Duel.HintSelection(tc)
 		Duel.Destroy(tc,REASON_EFFECT)
