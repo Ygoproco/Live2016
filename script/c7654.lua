0ff5b4dc6642597961e5897be3880d09b5f94f3d
EerieCode <eeriecode@gmail.com>
2016-10-21 07:31:32 +0200

    Fixed "Speed Drift" (credits to mercury233)

--- script/c7654.lua
+++ script/c7654.lua
@@ -1,50 +1,61 @@
 --スピードリフト
 --Speed Drift
---Scripted by Eerie Code
+--Script by mercury233
 function c7654.initial_effect(c)
-	--
+	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCondition(c7654.con)
-	e1:SetTarget(c7654.tg)
-	e1:SetOperation(c7654.op)
+	e1:SetCondition(c7654.condition)
+	e1:SetTarget(c7654.target)
+	e1:SetOperation(c7654.activate)
 	c:RegisterEffect(e1)
 end
-
-function c7654.cfil(c)
+function c7654.condition(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(nil,tp,LOCATION_MZONE,0,nil)
+	if g:GetCount()~=1 then return false end
+	local c=g:GetFirst()
 	return c:IsFaceup() and c:IsType(TYPE_TUNER)
 end
-function c7654.con(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==1 and Duel.IsExistingMatchingCard(c7654.cfil,tp,LOCATION_MZONE,0,1,nil)
-end
-function c7654.fil(c,e,tp)
+function c7654.spfilter(c,e,tp)
 	return c:IsSetCard(0x2016) and c:IsLevelBelow(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c7654.tg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(c7654.fil,tp,LOCATION_DECK,0,1,nil,e,tp) end
+function c7654.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c7654.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
-function c7654.op(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
+function c7654.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c7654.fil,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,c7654.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
 	local tc=g:GetFirst()
 	if tc and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
-		local e4=Effect.CreateEffect(c)
-		e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-		e4:SetCode(EVENT_SPSUMMON_SUCCESS)
-		e4:SetOperation(c7654.sumsuc)
-		tc:RegisterEffect(e4)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+		e1:SetLabelObject(tc)
+		e1:SetOperation(c7654.sumop)
+		Duel.RegisterEffect(e1,tp)
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e2:SetCode(EVENT_CHAIN_END)
+		e2:SetLabelObject(e1)
+		e2:SetOperation(c7654.cedop)
+		Duel.RegisterEffect(e2,tp)
+		Duel.SpecialSummonComplete()
 	end
 end
-function c7654.genchainlm(c)
-	return  function (e,rp,tp)
-				return e:GetHandler()==c
-			end
+function c7654.sumop(e,tp,eg,ep,ev,re,r,rp)
+	if eg:IsContains(e:GetLabelObject()) then
+		e:SetLabel(1)
+		e:Reset()
+	else e:SetLabel(0) end
 end
-function c7654.sumsuc(e,tp,eg,ep,ev,re,r,rp)
-	Duel.SetChainLimitTillChainEnd(c7654.genchainlm(e:GetHandler()))
+function c7654.cedop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.CheckEvent(EVENT_SPSUMMON_SUCCESS) and e:GetLabelObject():GetLabel()==1 then
+		Duel.SetChainLimitTillChainEnd(aux.FALSE)
+	end
+	e:Reset()
 end
