e8438e25a237145825544b05fcc245d1d2660ba3
senpaizuri3 <senpaizuri3@gmail.com>
2016-10-23 16:05:28 -0400

    Add files via upload

---
+++ script/c511004670.lua
@@ -0,0 +1,19 @@
+--Speedroid Terrortop (Anime)
+--Made by: senpaizuri3
+function c511004670.initial_effect(c)
+	c:EnableReviveLimit()
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(c511004670.spcon)
+	c:RegisterEffect(e1)
+end
+
+function c511004670.spcon(e,c)
+	if c==nil then return true end
+	return Duel.GetFieldGroupCount(c:GetControler(),LOCATION_MZONE,0,nil)==0
+		and Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
+end
