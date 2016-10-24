866dcfcae1e400a4174d3f3a359650ead7ad87d8
Michael Lawrence C. Dee <michael_lawrence_dee@yahoo.com>
2016-10-25 01:01:25 +0800

    Anime Card Fixes
    Boss Duel - draw phase losing, fixed.
    Don Thousand/Spell A - allows using Don Thousand/Trap A and Don
    Thousand/Trap B
    Don Thousand/Trap A - checks for being selected by Numeron Network
    Additional Check for regular Anime Numeron Network
    Numeron Spell Revision - fixed issue with Negating during cost
    Performapal Performage Wing Sandwichman - effect update (but requires
    core modification and update to completely fix Pendulum Scale issue)
    Raidraptor Target Flag fix
    Ice Edge unaffected by Level 4 or higher fixed
    Freedom Bird fixed
    Rank-Up-Magic Escape Force error when there is no attack target fixed +
    C88 OCG checking
    The Phantom Knights of Double Badge fix
    The Phantom Knights of Around Burn fix
    Bloom Diva Material fix (but requires database fix to fix archetype)

--- script/c95000023.lua
+++ script/c95000023.lua
@@ -23,6 +23,15 @@ function c95000023.initial_effect(c)
 	e3:SetCost(c95000023.accost)
 	e3:SetOperation(c95000023.acop)
 	c:RegisterEffect(e3)
+	local e6=Effect.CreateEffect(c)
+	e6:SetDescription(aux.Stringid(93016201,0))
+	e6:SetType(EFFECT_TYPE_QUICK_O)
+	e6:SetCode(EVENT_SPSUMMON)
+	e6:SetRange(LOCATION_SZONE)
+	e6:SetCondition(c95000023.accon)
+	e6:SetTarget(c95000023.accost)
+	e6:SetOperation(c95000023.acop)
+	c:RegisterEffect(e6)
 end
 c95000023.mark=0
 function c95000023.valcon(e,re,r,rp)
@@ -31,28 +40,59 @@ end
 function c95000023.accon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetFieldGroupCount(tp,LOCATION_ONFIELD,0)<=1
 end
-function c95000023.cfilter(c)
-	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToGrave() and c:CheckActivateEffect(false,false,false)~=nil
+function c95000023.cfilter(c,e,tp,eg,ep,ev,re,r,rp,chain)
+	local te=c:GetActivateEffect()
+	if not te or not c:IsType(TYPE_SPELL+TYPE_TRAP) or not c:IsAbleToGraveAsCost() then return false end
+	local condition=te:GetCondition()
+	local cost=te:GetCost()
+	local target=te:GetTarget()
+	if te:GetCode()==EVENT_CHAINING then
+		if chain<=0 then return false end
+		local te2=Duel.GetChainInfo(chain,CHAININFO_TRIGGERING_EFFECT)
+		local tc=te2:GetHandler()
+		local g=Group.FromCards(tc)
+		local p=tc:GetControler()
+		return (not condition or condition(e,tp,g,p,chain,te2,REASON_EFFECT,p)) and (not cost or cost(e,tp,g,p,chain,te2,REASON_EFFECT,p,0)) 
+			and (not target or target(e,tp,g,p,chain,te2,REASON_EFFECT,p,0))
+	elseif (te:GetCode()==EVENT_SPSUMMON and e:GetCode()==EVENT_SPSUMMON) or (e:GetCode()==EVENT_FREE_CHAIN and te:GetCode()~=EVENT_SPSUMMON) then
+		return (not condition or condition(e,tp,eg,ep,ev,re,r,rp)) and (not cost or cost(e,tp,eg,ep,ev,re,r,rp,0)) 
+			and (not target or target(e,tp,eg,ep,ev,re,r,rp,0))
+	end
 end
 function c95000023.accost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c95000023.cfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil) end
+	local chain=Duel.GetCurrentChain()
+	if chk==0 then return Duel.IsExistingMatchingCard(c95000023.cfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp,eg,ep,ev,re,r,rp,chain) end
+	chain=chain-1
+	Duel.RegisterFlagEffect(tp,511001666,RESET_CHAIN,0,1)
+	Duel.RegisterFlagEffect(tp,95000027,RESET_CHAIN,0,1)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,c95000023.cfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,c95000023.cfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp,eg,ep,ev,re,r,rp,chain)
 	Duel.SendtoGrave(g,REASON_COST)
 	Duel.SetTargetCard(g:GetFirst())
 end
 function c95000023.acop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc then
-		local te=tc:CheckActivateEffect(false,false,false)
+		local te=tc:GetActivateEffect()
 		if not te then return end
 		e:SetCategory(te:GetCategory())
 		e:SetProperty(te:GetProperty())
 		local co=te:GetCost()
 		local tg=te:GetTarget()
 		local op=te:GetOperation()
-		if co then co(e,tp,eg,ep,ev,re,r,rp,1) end
-		if tg then tg(e,tp,eg,ep,ev,re,r,rp,1) end
-		if op then op(e,tp,eg,ep,ev,re,r,rp) end
+		if te:GetCode()==EVENT_CHAINING then
+			local chain=Duel.GetCurrentChain()-1
+			local te2=Duel.GetChainInfo(chain,CHAININFO_TRIGGERING_EFFECT)
+			local tc=te2:GetHandler()
+			local g=Group.FromCards(tc)
+			local p=tc:GetControler()
+			if co then co(e,tp,g,p,chain,te2,REASON_EFFECT,p,1) end
+			if tg then tg(e,tp,g,p,chain,te2,REASON_EFFECT,p,1) end
+			if op then op(e,tp,g,p,chain,te2,REASON_EFFECT,p) end
+		else
+			if co then co(e,tp,eg,ep,ev,re,r,rp,1) end
+			if tg then tg(e,tp,eg,ep,ev,re,r,rp,1) end
+			if op then op(e,tp,eg,ep,ev,re,r,rp) end
+		end
 	end
 end
