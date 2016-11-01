62fd182be86b40de48a1d514e0984ebf3e90ce2c
EerieCode <eeriecode@gmail.com>
2016-11-01 18:27:06 +0100

    Official numbers for INOV TCG-exclusives

--- script/c66698383.lua
+++ script/c66698383.lua
@@ -1,6 +1,6 @@
 --Vermillion Dragon Mech
 --Scripted by Eerie Code
-function c7481.initial_effect(c)
+function c66698383.initial_effect(c)
 	c:EnableReviveLimit()
 	aux.AddSynchroProcedure(c,nil,aux.NonTuner(nil),1)
 	--
@@ -10,9 +10,9 @@ function c7481.initial_effect(c)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
-	e1:SetCost(c7481.descost)
-	e1:SetTarget(c7481.destg)
-	e1:SetOperation(c7481.desop)
+	e1:SetCost(c66698383.descost)
+	e1:SetTarget(c66698383.destg)
+	e1:SetOperation(c66698383.desop)
 	c:RegisterEffect(e1)
 	--
 	local e2=Effect.CreateEffect(c)
@@ -21,51 +21,51 @@ function c7481.initial_effect(c)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetCode(EVENT_TO_GRAVE)
 	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
-	e2:SetCondition(c7481.thcon)
-	e2:SetTarget(c7481.thtg)
-	e2:SetOperation(c7481.thop)
+	e2:SetCondition(c66698383.thcon)
+	e2:SetTarget(c66698383.thtg)
+	e2:SetOperation(c66698383.thop)
 	c:RegisterEffect(e2)
 end
 
-function c7481.descfil(c)
+function c66698383.descfil(c)
 	return c:IsType(TYPE_TUNER) and c:IsAbleToRemoveAsCost()
 end
-function c7481.descost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c7481.descfil,tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE,0,1,nil) end
+function c66698383.descost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c66698383.descfil,tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,c7481.descfil,tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,c66698383.descfil,tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE,0,1,1,nil)
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
-function c7481.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function c66698383.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsOnField() and chkc:IsDestructable() end
 	if chk==0 then return Duel.IsExistingTarget(Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectTarget(tp,Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
-function c7481.desop(e,tp,eg,ep,ev,re,r,rp)
+function c66698383.desop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
 		Duel.Destroy(tc,REASON_EFFECT)
 	end
 end
 
-function c7481.thcon(e,tp,eg,ep,ev,re,r,rp)
+function c66698383.thcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return c:IsReason(REASON_DESTROY) and c:IsReason(REASON_EFFECT)
 		and c:IsPreviousLocation(LOCATION_MZONE) and c:IsSummonType(SUMMON_TYPE_SYNCHRO)
 end
-function c7481.thfil(c)
+function c66698383.thfil(c)
 	return c:IsFaceup() and c:IsType(TYPE_TUNER) and c:IsAbleToHand()
 end
-function c7481.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_REMOVED) and c7481.thfil(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c7481.thfil,tp,LOCATION_REMOVED,0,1,nil) end
+function c66698383.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_REMOVED) and c66698383.thfil(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c66698383.thfil,tp,LOCATION_REMOVED,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectTarget(tp,c7481.thfil,tp,LOCATION_REMOVED,0,1,1,nil)
+	local g=Duel.SelectTarget(tp,c66698383.thfil,tp,LOCATION_REMOVED,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
 end
-function c7481.thop(e,tp,eg,ep,ev,re,r,rp)
+function c66698383.thop(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
