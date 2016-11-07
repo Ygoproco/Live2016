e75f398648ef980087cc5f26a394c37965cf9cf5
TheOnePharaoh <TheOnePharaoh@users.noreply.github.com>
2016-11-07 19:07:52 +0200

    Fixed the Rank Up check with OCG ruling.
    Fixed the Chaos Numbers and CXyz Rank up effect.

--- script/c511001429.lua
+++ script/c511001429.lua
@@ -6,7 +6,6 @@ function c511001429.initial_effect(c)
 	--Rank Up Check
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetCondition(c511001429.rankupregcon)
 	e1:SetOperation(c511001429.rankupregop)
--- script/c513000063.lua
+++ script/c513000063.lua
@@ -6,7 +6,6 @@ function c513000063.initial_effect(c)
 	--Rank Up Check
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetOperation(c513000063.rankupregop)
 	c:RegisterEffect(e1)
