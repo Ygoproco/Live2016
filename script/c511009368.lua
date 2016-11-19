3c85a9ac8895c8153343516117e6f40aff0b776d
TheOnePharaoh <TheOnePharaoh@users.noreply.github.com>
2016-11-19 08:58:38 -0600

    More Fixes

--- script/c511009368.lua
+++ script/c511009368.lua
@@ -138,14 +138,11 @@ end
 function c511009368.condition(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():GetFlagEffect(511009368)~=0
 end
-function c511009368.desfilter(c)
-	return c:IsDestructable()
-end
 function c511009368.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and c511009368.desfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c511009368.desfilter,tp,0,LOCATION_MZONE,1,nil) end
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) end
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,c511009368.desfilter,tp,0,LOCATION_MZONE,1,1,nil)
+	local g=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_MZONE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,g:GetFirst():GetAttack())
 end
