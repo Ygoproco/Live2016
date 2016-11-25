266a2c20d615f1e1d6e8e6de05e7f7b883c0bda8
EerieCode <eeriecode@gmail.com>
2016-11-25 20:43:24 +0100

    Official numbers for Dimension Box

--- script/c34773082.lua
+++ script/c34773082.lua
@@ -1,32 +1,32 @@
 --魔玩具補綴
 --Frightfur Patchwork
 --Scripted by Eerie Code
-function c7672.initial_effect(c)
+function c34773082.initial_effect(c)
 	--
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCountLimit(1,7672+EFFECT_COUNT_CODE_OATH)
-	e1:SetTarget(c7672.target)
-	e1:SetOperation(c7672.activate)
+	e1:SetCountLimit(1,34773082+EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(c34773082.target)
+	e1:SetOperation(c34773082.activate)
 	c:RegisterEffect(e1)
 end
 
-function c7672.filter1(c)
+function c34773082.filter1(c)
 	return c:IsCode(24094653) and c:IsAbleToHand()
 end
-function c7672.filter2(c)
+function c34773082.filter2(c)
 	return c:IsSetCard(0xc3) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
 end
-function c7672.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c7672.filter1,tp,LOCATION_DECK,0,1,nil)
-		and Duel.IsExistingMatchingCard(c7672.filter2,tp,LOCATION_DECK,0,1,nil) end
+function c34773082.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c34773082.filter1,tp,LOCATION_DECK,0,1,nil)
+		and Duel.IsExistingMatchingCard(c34773082.filter2,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_DECK)
 end
-function c7672.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g1=Duel.GetMatchingGroup(c7672.filter1,tp,LOCATION_DECK,0,nil)
-	local g2=Duel.GetMatchingGroup(c7672.filter2,tp,LOCATION_DECK,0,nil)
+function c34773082.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g1=Duel.GetMatchingGroup(c34773082.filter1,tp,LOCATION_DECK,0,nil)
+	local g2=Duel.GetMatchingGroup(c34773082.filter2,tp,LOCATION_DECK,0,nil)
 	if g1:GetCount()>0 and g2:GetCount()>0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 		local sg1=g1:Select(tp,1,1,nil)
