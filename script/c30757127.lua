dbdfe4e8a92571646add4895c6d7668340a43971
EerieCode <eeriecode@gmail.com>
2016-12-11 10:54:02 +0100

    Fixed "Destiny HERO - Deadlyguy"

--- script/c30757127.lua
+++ script/c30757127.lua
@@ -2,7 +2,14 @@
 function c30757127.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
-	aux.AddFusionProcFun2(c,aux.FilterBoolFunction(Card.IsFusionSetCard,0xc008),c30757127.ffilter,true)
+	--aux.AddFusionProcFun2(c,aux.FilterBoolFunction(Card.IsFusionSetCard,0xc008),c30757127.ffilter,true)
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCode(EFFECT_FUSION_MATERIAL)
+	e0:SetCondition(c30757127.funcon)
+	e0:SetOperation(c30757127.funop)
+	c:RegisterEffect(e0)
 	--atk
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(30757127,0))
@@ -20,12 +27,73 @@ function c30757127.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 c30757127.material_setcode=0x8
-function c30757127.ffilter(c)
+function c30757127.mat_fil_temp(c,fc)
+	local attr=c:IsAttribute(ATTRIBUTE_DARK)
 	if Card.IsFusionAttribute then
-		return c:IsFusionAttribute(ATTRIBUTE_DARK) and c:IsType(TYPE_EFFECT)
-	else
-		return c:IsAttribute(ATTRIBUTE_DARK) and c:IsType(TYPE_EFFECT)
+		attr=c:IsFusionAttribute(ATTRIBUTE_DARK,fc)
 	end
+	return attr and c:IsType(TYPE_EFFECT)
+end
+function c30757127.funcon(e,g,gc,chkfnf)
+	if g==nil then return true end
+	local c=e:GetHandler()
+	local f2=aux.FilterBoolFunction(Card.IsFusionSetCard,0xc008)
+	local chkf=bit.band(chkfnf,0xff)
+	local mg=g:Filter(Card.IsCanBeFusionMaterial,nil,e:GetHandler())
+	if gc then
+		if not gc:IsCanBeFusionMaterial(e:GetHandler()) then return false end
+		return (c30757127.mat_fil_temp(gc,c) and mg:IsExists(f2,1,gc,c))
+			or (f2(gc) and mg:IsExists(c30757127.mat_fil_temp,1,gc,c)) end
+	local g1=Group.CreateGroup() local g2=Group.CreateGroup() local fs=false
+	local tc=mg:GetFirst()
+	while tc do
+		if c30757127.mat_fil_temp(tc,c) then g1:AddCard(tc) if Auxiliary.FConditionCheckF(tc,chkf) then fs=true end end
+		if f2(tc) then g2:AddCard(tc) if Auxiliary.FConditionCheckF(tc,chkf) then fs=true end end
+		tc=mg:GetNext()
+	end
+	if chkf~=PLAYER_NONE then
+		return fs and g1:IsExists(Auxiliary.FConditionFilterF2,1,nil,g2)
+	else return g1:IsExists(Auxiliary.FConditionFilterF2,1,nil,g2) end
+end
+function c30757127.funop(e,tp,eg,ep,ev,re,r,rp,gc,chkfnf)
+	local chkf=bit.band(chkfnf,0xff)
+	local c=e:GetHandler()
+	local f2=aux.FilterBoolFunction(Card.IsFusionSetCard,0xc008)
+	local g=eg:Filter(Card.IsCanBeFusionMaterial,nil,e:GetHandler())
+	if gc then
+		local sg=Group.CreateGroup()
+		if c30757127.mat_fil_temp(gc,c) then sg:Merge(g:Filter(f2,gc,c)) end
+		if f2(gc) then sg:Merge(g:Filter(c30757127.mat_fil_temp,gc,c)) end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FMATERIAL)
+		local g1=sg:Select(tp,1,1,nil)
+		Duel.SetFusionMaterial(g1)
+		return
+	end
+	local sg=g:Filter(c30757127.FConditionFilterF2c,nil,c)
+	local g1=nil
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FMATERIAL)
+	if chkf~=PLAYER_NONE then
+		g1=sg:FilterSelect(tp,Auxiliary.FConditionCheckF,1,1,nil,chkf)
+	else g1=sg:Select(tp,1,1,nil) end
+	local tc1=g1:GetFirst()
+	sg:RemoveCard(tc1)
+	local b1=c30757127.mat_fil_temp(tc1,c)
+	local b2=f2(tc1)
+	if b1 and not b2 then sg:Remove(c30757127.FConditionFilterF2r1,nil,c) end
+	if b2 and not b1 then sg:Remove(c30757127.FConditionFilterF2r2,nil,c) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FMATERIAL)
+	local g2=sg:Select(tp,1,1,nil)
+	g1:Merge(g2)
+	Duel.SetFusionMaterial(g1)
+end
+function c30757127.FConditionFilterF2c(c,fc)
+	return c30757127.mat_fil_temp(c,fc) or c:IsFusionSetCard(0xc008)
+end
+function c30757127.FConditionFilterF2r1(c,fc)
+	return c30757127.mat_fil_temp(c,fc) and not c:IsFusionSetCard(0xc008)
+end
+function c30757127.FConditionFilterF2r2(c,fc)
+	return c:IsFusionSetCard(0xc008) and not c30757127.mat_fil_temp(c,fc)
 end
 function c30757127.atkcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetCurrentPhase()~=PHASE_DAMAGE or not Duel.IsDamageCalculated()
