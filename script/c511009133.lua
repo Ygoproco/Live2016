1a1c0eb426f3172630718574f50cf6aa6a1b601b
TheOnePharaoh <alaaz018@yahoo.com>
2016-12-11 06:46:36 -0600

    Moved #339 Scripts to the right folder

--- script/c511009133.lua
+++ script/c511009133.lua
@@ -25,14 +25,12 @@ function c511009133.atkcon(e,tp,eg,ep,ev,re,r,rp)
 	local phase=Duel.GetCurrentPhase()
 	if Duel.IsDamageCalculated() then return false end
 	local tc=Duel.GetAttacker()
-	local d=Duel.GetAttackTarget()
 	if not tc then return false end
 	if tc:IsControler(1-tp) then 
 	tc=Duel.GetAttackTarget() 
-	d=Duel.GetAttacker()
 	end
 	e:SetLabelObject(tc)
-	return d and tc and tc:IsFaceup() and tc:IsSetCard(0xe5) and tc:IsRelateToBattle()  and d:IsRelateToBattle() 
+	return tc and tc:IsFaceup() and tc:IsSetCard(0xe5) and tc:IsRelateToBattle()  and d:IsRelateToBattle() 
 	and Duel.IsExistingMatchingCard(c511009133.filter,0,LOCATION_MZONE,LOCATION_MZONE,1,tc,tc:GetCode())
 		and not e:GetHandler():IsStatus(STATUS_CHAINING)
 end
