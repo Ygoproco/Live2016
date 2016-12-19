e12f6edc36c4cb7fb83a5443f682dc17cc0cbef3
TheOnePharaoh <TheOnePharaoh@users.noreply.github.com>
2016-12-19 08:59:24 -0600

    Supreme King Dragon Zarc

--- script/c100912039.lua
+++ script/c100912039.lua
@@ -3,7 +3,7 @@
 --Scripted by Eerie Code
 function c100912039.initial_effect(c)
 	c:EnableReviveLimit()
-	--aux.AddFusionProcFunMulti(c,true,c100912039.filters)
+	--aux.AddFusionProcFunMulti(c,false,c100912039.filters)
 	aux.EnablePendulumAttribute(c,false)
 	--fusion procedure
 	local e0=Effect.CreateEffect(c)
@@ -210,7 +210,7 @@ end
 function c100912039.fuscon(e,g,gc,chkfnf)
 	local c=e:GetHandler()
 	if g==nil then return true end
-	if not c:IsFacedown() then return false end
+	if c:IsFaceup() then return false end
 	local chkf=bit.band(chkfnf,0xff)
 	local funs=c100912039.filters
 	local n=4
@@ -243,9 +243,10 @@ function c100912039.fuscon(e,g,gc,chkfnf)
 		end
 		tc=mg:GetNext()
 	end
+	local gr2=c100912039.CloneTable(groups)
 	if chkf~=PLAYER_NONE then
-		if not fs then return false end
-		local gr2=c100912039.CloneTable(groups)
+		return fs and gr2[1]:IsExists(c100912039.FConditionFilterMulti2,1,nil,gr2)
+	else
 		return gr2[1]:IsExists(c100912039.FConditionFilterMulti2,1,nil,gr2)
 	end
 end
@@ -396,3 +397,4 @@ function c100912039.penop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.MoveToField(c,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
 	end
 end
+
