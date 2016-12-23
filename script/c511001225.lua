00c38ae0c56215dc0329443a4874877e42722c5b
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2016-12-23 22:56:34 +0800

    Anime Card Fixes pt. 7
    Special Xyz Summon Procedure update

--- script/c511001225.lua
+++ script/c511001225.lua
@@ -15,7 +15,7 @@ function c511001225.initial_effect(c)
 			mt.nontuner_filter=function(mc) return true end
 		end
 		mt.minntct=ct
-		mt.maxntct=99
+		mt.maxntct=63
 		mt.sync=true
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_FIELD)
@@ -59,17 +59,14 @@ function c511001225.initial_effect(c)
 	function aux.AddXyzProcedure(c,f,lv,ct,alterf,desc,maxct,op)
 		local code=c:GetOriginalCode()
 		local mt=_G["c" .. code]
-		if f then
-			mt.xyz_filter=function(mc) return mc and f(mc) end
-		else
-			mt.xyz_filter=function(mc) return true end
-		end
+		mt.xyz_filter=function(mc) return mc and (not f or f(mc)) and mc:IsXyzLevel(c,lv) and not mc:IsType(TYPE_TOKEN) end
 		mt.minxyzct=ct
 		if not maxct then
 			mt.maxxyzct=ct
 		else
 			if maxct==5 and code~=14306092 and code~=63504681 and code~=23776077 then
-				mt.maxxyzct=99
+				maxct=63
+				mt.maxxyzct=63
 			else
 				mt.maxxyzct=maxct
 			end
@@ -168,6 +165,10 @@ function c511001225.operation(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function c511001225.xyzchk(e,tp,eg,ep,ev,re,r,rp)
-	Duel.CreateToken(tp,419)
-	Duel.CreateToken(1-tp,419)
+	if Duel.GetFlagEffect(tp,419)==0 and Duel.GetFlagEffect(1-tp,419)==0 then
+		Duel.CreateToken(tp,419)
+		Duel.CreateToken(1-tp,419)
+		Duel.RegisterFlagEffect(tp,419,nil,0,1)
+		Duel.RegisterFlagEffect(1-tp,419,nil,0,1)
+	end
 end
--- script/c511002733.lua
+++ script/c511002733.lua
@@ -3,17 +3,14 @@ function c511002733.initial_effect(c)
 	function aux.AddXyzProcedure(c,f,lv,ct,alterf,desc,maxct,op)
 		local code=c:GetOriginalCode()
 		local mt=_G["c" .. code]
-		if f then
-			mt.xyz_filter=function(mc) return mc and f(mc) end
-		else
-			mt.xyz_filter=function(mc) return true end
-		end
+		mt.xyz_filter=function(mc) return mc and (not f or f(mc)) and mc:IsXyzLevel(c,lv) and not mc:IsType(TYPE_TOKEN) end
 		mt.minxyzct=ct
 		if not maxct then
 			mt.maxxyzct=ct
 		else
 			if maxct==5 and code~=14306092 and code~=63504681 and code~=23776077 then
-				mt.maxxyzct=99
+				maxct=63
+				mt.maxxyzct=63
 			else
 				mt.maxxyzct=maxct
 			end
@@ -57,8 +54,12 @@ function c511002733.initial_effect(c)
 	end
 end
 function c511002733.xyzchk(e,tp,eg,ep,ev,re,r,rp)
-	Duel.CreateToken(tp,419)
-	Duel.CreateToken(1-tp,419)
+	if Duel.GetFlagEffect(tp,419)==0 and Duel.GetFlagEffect(1-tp,419)==0 then
+		Duel.CreateToken(tp,419)
+		Duel.CreateToken(1-tp,419)
+		Duel.RegisterFlagEffect(tp,419,nil,0,1)
+		Duel.RegisterFlagEffect(1-tp,419,nil,0,1)
+	end
 end
 function c511002733.filter(c,e,tp)
 	return c:IsLevelAbove(8) and c:GetLevel()%2==0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
