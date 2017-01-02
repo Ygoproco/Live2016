394f6913b8af652bd7ca7eed66920630ea7e8ec0
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-01-02 19:42:55 +0800

    Anime Card Fixes
    Battlewasp Ballista changed to generic materials (database text not yet included)
    Different Dimension Barrier - Lost Force checking of leaving controller's field
    Black Feather Cursed Guard checking of face-up monster added
    Zombie Heart script fix (database text not yet included)
    Xyz Battle Chain script fix

--- script/c511001948.lua
+++ script/c511001948.lua
@@ -2,15 +2,7 @@
 function c511001948.initial_effect(c)
 	--synchro summon
 	c:EnableReviveLimit()
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_SPSUMMON_PROC)
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_IMMUNE)
-	e1:SetRange(LOCATION_EXTRA)
-	e1:SetCondition(c511001948.syncon)
-	e1:SetOperation(c511001948.synop)
-	e1:SetValue(SUMMON_TYPE_SYNCHRO)
-	c:RegisterEffect(e1)
+	aux.AddSynchroProcedure(c,nil,aux.NonTuner(nil),1)
 	--atk down
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(511001948,0))
@@ -33,62 +25,6 @@ function c511001948.initial_effect(c)
 end
 function c511001948.atkcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetCurrentPhase()~=PHASE_DAMAGE or not Duel.IsDamageCalculated()
-end
-function c511001948.matfilter(c,syncard)
-	return c:IsFaceup() and c:IsCanBeSynchroMaterial(syncard)
-end
-function c511001948.synfilter1(c,syncard,lv,g)
-	local tlv=c:GetSynchroLevel(syncard)
-	if lv-tlv<=0 then return false end
-	if c:IsNotTuner() then return false end
-	local wg=g:Clone()
-	wg:RemoveCard(c)
-	return wg:IsExists(c511001948.synfilter2,1,nil,syncard,lv-tlv,wg)
-end
-function c511001948.synfilter2(c,syncard,lv,g)
-	if not c:IsSetCard(0x214) or not c:IsType(TYPE_SYNCHRO) then return false end
-	local tlv=c:GetSynchroLevel(syncard)
-	if lv-tlv<=0 then return false end
-	return g:IsExists(c511001948.synfilter3,1,c,syncard,lv-tlv)
-end
-function c511001948.synfilter3(c,syncard,lv)
-	local mlv=c:GetSynchroLevel(syncard)
-	local lv1=bit.band(mlv,0xffff)
-	local lv2=bit.rshift(mlv,16)
-	return c:IsNotTuner() and (lv1==lv or lv2==lv)
-end
-function c511001948.syncon(e,c,tuner)
-	if c==nil then return true end
-	local tp=c:GetControler()
-	local mg=Duel.GetMatchingGroup(c511001948.matfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,c)
-	local lv=c:GetLevel()
-	if tuner then return c511001948.synfilter1(tuner,c,lv,mg) end
-	return mg:IsExists(c511001948.synfilter1,1,nil,c,lv,mg)
-end
-function c511001948.synop(e,tp,eg,ep,ev,re,r,rp,c,tuner)
-	local g=Group.CreateGroup()
-	local mg=Duel.GetMatchingGroup(c511001948.matfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,c)
-	local lv=c:GetLevel()
-	local m1=tuner
-	if not tuner then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SMATERIAL)
-		local t1=mg:FilterSelect(tp,c511001948.synfilter1,1,1,nil,c,lv,mg)
-		m1=t1:GetFirst()
-		g:AddCard(m1)
-	end
-	lv=lv-m1:GetSynchroLevel(c)
-	mg:RemoveCard(m1)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SMATERIAL)
-	local t2=mg:FilterSelect(tp,c511001948.synfilter2,1,1,nil,c,lv,mg)
-	local m2=t2:GetFirst()
-	g:AddCard(m2)
-	lv=lv-m2:GetSynchroLevel(c)
-	mg:RemoveCard(m2)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SMATERIAL)
-	local t3=mg:FilterSelect(tp,c511001948.synfilter3,1,1,nil,c,lv)
-	g:Merge(t3)
-	c:SetMaterial(g)
-	Duel.SendtoGrave(g,REASON_MATERIAL+REASON_SYNCHRO)
 end
 function c511001948.cfilter(c)
 	return c:IsSetCard(0x214) and c:IsType(TYPE_MONSTER)
