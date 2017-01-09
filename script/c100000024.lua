4287071b0213e2a10a1c329d064d943c919ccc61
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-01-09 13:51:16 +0800

    Time Stream update (database fix not yet included)

--- script/c100000024.lua
+++ script/c100000024.lua
@@ -13,27 +13,21 @@ function c100000024.initial_effect(c)
 end
 function c100000024.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	local lp=Duel.GetLP(tp)
-	local costlp=math.floor((lp+1)/2)
-	Duel.PayLPCost(tp,costlp)
-end
-function c100000024.tfilter1(c,e,tp)
-	local code=c:GetCode()
-	return (code==100000027 or code==100000622) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,true,false)
-end
-function c100000024.tfilter2(c,e,tp)
-	local code=c:GetCode()
-	return (code==100000028 or code==100000023 or code==100000029) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,true,false)
+	Duel.PayLPCost(tp,Duel.GetLP(tp)/2)
 end
 function c100000024.filter(c,e,tp)
 	local code=c:GetCode()
-	return c:IsFaceup() and (((code==100000027 or code==100000622)
-		and Duel.IsExistingMatchingCard(c100000024.tfilter2,tp,LOCATION_EXTRA,0,1,nil,e,tp))
-		or ((code==100000026 or code==100000620 or code==100000621)
-		and Duel.IsExistingMatchingCard(c100000024.tfilter1,tp,LOCATION_EXTRA,0,1,nil,e,tp)))
+	return c:IsFaceup() and c:IsAbleToExtra() 
+		and Duel.IsExistingMatchingCard(c100000024.spfilter,tp,LOCATION_EXTRA,0,1,c,e,tp,c)
+end
+function c100000024.spfilter(c,e,tp,rc)
+	if not c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,true,false) then return false end
+	local res1=c:IsCode(100000027) and rc:IsCode(100000026)
+	local res2=c:IsCode(100000028) and rc:IsCode(100000027)
+	return res1 or res2
 end
 function c100000024.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and c100000024.filter1(chkc,e,tp) or c100000024.filter2(chkc,e,tp)end
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and c100000024.filter(chkc,e,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1
 		and Duel.IsExistingTarget(c100000024.filter,tp,LOCATION_MZONE,0,1,nil,e,tp)  end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
@@ -42,14 +36,11 @@ function c100000024.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function c100000024.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if not tc:IsRelateToEffect(e) then return end
-	if Duel.SendtoDeck(tc,nil,1,REASON_EFFECT)==0 then return end
+	if not tc or not tc:IsRelateToEffect(e) or Duel.SendtoDeck(tc,nil,0,REASON_EFFECT)==0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local code=tc:GetCode()
-	if code==100000026 or code==100000620 or code==100000621 then 
-	local sg=Duel.SelectMatchingCard(tp,c100000024.tfilter1,tp,LOCATION_EXTRA,0,1,1,nil,e,tp) 
-	Duel.SpecialSummon(sg,SUMMON_TYPE_FUSION,tp,tp,true,false,POS_FACEUP) end
-	if code==100000027 or code==100000622 then 
-	local sg=Duel.SelectMatchingCard(tp,c100000024.tfilter2,tp,LOCATION_EXTRA,0,1,1,nil,e,tp) 
-	Duel.SpecialSummon(sg,SUMMON_TYPE_FUSION,tp,tp,true,false,POS_FACEUP) end
+	local sg=Duel.SelectMatchingCard(tp,c100000024.spfilter,tp,LOCATION_EXTRA,0,1,1,tc,e,tp,tc)
+	local sc=sg:GetFirst()
+	if sc and Duel.SpecialSummon(sc,SUMMON_TYPE_FUSION,tp,tp,true,false,POS_FACEUP)>0 then
+		sc:CompleteProcedure()
+	end
 end
