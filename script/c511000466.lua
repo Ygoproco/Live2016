15b24ee9b34fa450c53755958385ace9ef2a5c0f
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-01-11 13:16:14 +0800

    Anime Card Fixes
    Predaplant Cordyceps Anime effect update (database fix not yet included)
    Destiny Draw (rule) effect update (database fix not yet included)
    Cursebreaker script fix

--- script/c511000466.lua
+++ script/c511000466.lua
@@ -4,13 +4,13 @@ function c511000466.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_DISABLE+CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_CHAINING)
+	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetTarget(c511000466.target)
 	e1:SetOperation(c511000466.activate)
 	c:RegisterEffect(e1)
 end
 function c511000466.filter(c)
-	return c:IsFaceup() and c:IsType(TYPE_SPELL) and c:IsDestructable()
+	return c:IsFaceup() and c:IsType(TYPE_SPELL)
 end
 function c511000466.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c511000466.filter,tp,0,LOCATION_ONFIELD,1,nil) end
