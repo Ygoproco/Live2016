52cc5a64c6c8e395da3f55f65c3e424dccc1cd4d
AndreOliveiraMendes <ao_mendes@hotmail.com>
2017-01-16 16:12:00 -0200

    fix for BESP
    added the check

---
+++ script/c95178994.lua
@@ -0,0 +1,44 @@
+--ジャイアントウィルス
+function c95178994.initial_effect(c)
+	--battle destroyed
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(95178994,0))
+	e1:SetCategory(CATEGORY_DAMAGE+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_BATTLE_DESTROYED)
+	e1:SetCondition(c95178994.condition)
+	e1:SetTarget(c95178994.target)
+	e1:SetOperation(c95178994.operation)
+	c:RegisterEffect(e1)
+end
+function c95178994.condition(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsLocation(LOCATION_GRAVE)
+		and e:GetHandler():IsReason(REASON_BATTLE)
+end
+function c95178994.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,500)
+end
+function c95178994.filter(c,e,tp)
+	return c:IsCode(95178994) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c95178994.operation(e,tp,eg,ep,ev,re,r,rp)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	Duel.Damage(1-tp,500,REASON_EFFECT)
+	if ft<=0 then return end
+	if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
+	local g=Duel.GetMatchingGroup(c95178994.filter,tp,LOCATION_DECK,0,nil,e,tp)
+	if g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(95178994,1)) then
+		Duel.BreakEffect()
+		local gc=g:GetFirst()
+		Duel.SpecialSummonStep(gc,0,tp,tp,false,false,POS_FACEUP_ATTACK)
+		ft=ft-1
+		gc=g:GetNext()
+		while ft>0 and gc and not Duel.IsPlayerAffectedByEffect(tp,59822133) and Duel.SelectYesNo(tp,aux.Stringid(95178994,1)) do
+			Duel.SpecialSummonStep(gc,0,tp,tp,false,false,POS_FACEUP_ATTACK)
+			ft=ft-1
+			gc=g:GetNext()
+		end
+		Duel.SpecialSummonComplete()
+	end
+end
